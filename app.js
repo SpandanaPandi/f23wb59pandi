3bea91eb592af0e2407ebfee1f5c7a6f16830f3c
SpandanaPandi <143204692+SpandanaPandi@users.noreply.github.com>
2023-11-01 21:22:27 -0500

    Added images

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var transportationRouter = require('./routes/transportation');
+var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 var app = express();
 
 // view engine setup
@@ -22,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/transportation', transportationRouter);
-
+app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
